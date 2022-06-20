6908050c51fe2a2e76fb59d937795bc2978e5007
Sergey Kuznetsov <mr.enline@gmail.com>
2022-06-20 18:36:46 +0300

    start

---
+++ server/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+//const mongoose = require('mongoose');
+
+const authRouter = require('./authRouter');
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+//const URL =
+//('mongodb+srv://admin:1@cluster0.kivb8.mongodb.net/auth_roles?retryWrites=true&w=majority');
+
+const app = express();
+
+app.use(express.json());
+app.use(
+    cors({
+        credentials: true,
+        origin: 'http://localhost:3000',
+        //origin: process.env.CLIENT_URL,
+    })
+);
+app.use('/auth', authRouter);
+
+const start = async () => {
+    try {
+        //await mongoose.connect(URL);
+        app.listen(PORT, () => {
+            console.log(`Server started on PORT ${PORT}`);
+        });
+    } catch (error) {
+        console.log();
+    }
+};
+
+start();

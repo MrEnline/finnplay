5602e5634485e3316591d573b3013aa58fd09806
Sergey Kuznetsov <mr.enline@gmail.com>
2022-06-21 19:01:52 +0300

    create role and return token

--- server/authController.js
+++ server/authController.js
@@ -1,91 +1,54 @@
-const Role = require('./models/Role.js');
-const User = require('./models/User.js');
-const bcrypt = require('bcryptjs');
+//const Role = require('./models/Role.js');
+//const User = require('./models/User.js');
+//const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { validationResult } = require('express-validator');
+//const { validationResult } = require('express-validator');
 const { secret } = require('./config.js');
+const { Users } = require('./Users');
 
-const generateAccessToken = (id, roles) => {
+const generateAccessToken = (id, role) => {
     const payLoad = {
         id,
-        roles,
+        role,
     };
     return jwt.sign(payLoad, secret, { expiresIn: '24h' });
 };
 
 class authController {
-    async registration(req, res) {
-        try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res
-                    .status(400)
-                    .json({ message: 'Error on registration' });
-            }
-            const { username, password } = req.body; //получаем имя и пароль из тела запроса
-            const candidate = await User.findOne({ username });
-            if (candidate) {
-                return res.status(400).json({
-                    message: 'A user with the same name already exists',
-                });
-            }
-            const hashPassword = bcrypt.hashSync(password, 7); //хэширование пароля
-            const userRole = await Role.findOne({ value: 'USER' });
-            const user = new User({
-                username,
-                password: hashPassword,
-                roles: [userRole.value],
-            });
-            await user.save(); //сохранение пользователя в БД
-            return res.json({
-                message: 'User successfully registered',
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(400).json({
-                message: 'Registration error',
-                e: error.message,
-            });
-        }
-    }
-
     async login(req, res) {
         try {
             const { username, password } = req.body;
-            const user = await User.findOne({ username }); //пытаемся получить пользователя из БД
+            const user = await Users[username];
             if (!user) {
                 return res
                     .status(400)
                     .json({ message: `${user} with this name not found` });
             }
-            //сравнение пароля, которое ввел пользователь и тем, который расположен в БД
-            const validPassword = await bcrypt.compareSync(
-                password,
-                user.password
-            );
+            const validPassword = await (password === user.password);
             if (!validPassword) {
                 return res
                     .status(400)
                     .json({ message: 'Wrong password entered' });
             }
-            //генерация токена, который затем будет отправлен пользователю
-            const token = generateAccessToken(user._id, user.roles);
-            return res.json({ username, token });
+            const token = generateAccessToken(user.id, user.adminRole);
+            const role = user.adminRole;
+            return res.json({ token, role });
         } catch (error) {
             res.status(400).json({ message: 'Login error', e: error.message });
         }
     }
 
-    async getUsers(req, res) {
+    async checkAuth(req, res) {}
+
+    async logout(req, res, next) {
         try {
-            //добавление ролей в БД. Таким путем лучше не делать никогда
-            // const userRole = new Role();
-            // const adminRole = new Role({ value: 'ADMIN' });
-            // await userRole.save();
-            // await adminRole.save();
-            const users = await User.find();
-            res.json(users);
-        } catch (error) {}
+            const { refreshToken } = req.cookies;
+            const token = await userService.logout(refreshToken);
+            res.clearCookie('refreshToken');
+            return res.json(token);
+        } catch (e) {
+            next(e);
+        }
     }
 }
 
--- server/authRouter.js
+++ server/authRouter.js
@@ -1,26 +1,15 @@
-const Router = require("express");
-const authController = require("./authController");
+const Router = require('express');
+const authController = require('./authController');
 const router = Router();
-const { check } = require("express-validator");
-const authMiddleware = require("./middlewaree/authMiddleware.js");
-const roleMiddleware = require("./middlewaree/roleMiddleware.js");
+//const { check } = require('express-validator');
+const authMiddleware = require('./middlewaree/authMiddleware.js');
+const roleMiddleware = require('./middlewaree/roleMiddleware.js');
 
-//данный роутер прослушивает маршруты, отмеченные ниже
-//и выполняет методы, которые задаются вторым параметром
-//check - функция middlewares, передается в массиве. Может быть больше чем 1
-router.post(
-    "/registration",
-    [
-        check("username", "Field cannot be empty").notEmpty(),
-        check(
-            "password",
-            "Password must be more than 4 and less than 12 characters",
-        ).isLength(4, 12),
-    ],
-    authController.registration,
-);
-router.post("/login", authController.login);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.get('/checkAuth', authController.checkAuth);
+
 //router.get("/users", authMiddleware, authController.getUsers);
-router.get("/users", roleMiddleware(["ADMIN"]), authController.getUsers);
+//router.get("/users", roleMiddleware(["ADMIN"]), authController.getUsers);
 
 module.exports = router;

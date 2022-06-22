8334231136e88e90bf879608a3bb3085a7d74148
Sergey Kuznetsov <mr.enline@gmail.com>
2022-06-22 18:18:00 +0300

    some changes

--- server/authRouter.js
+++ server/authRouter.js
@@ -1,15 +1,9 @@
 const Router = require('express');
 const authController = require('./authController');
 const router = Router();
-//const { check } = require('express-validator');
-const authMiddleware = require('./middlewaree/authMiddleware.js');
-const roleMiddleware = require('./middlewaree/roleMiddleware.js');
 
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.get('/checkAuth', authController.checkAuth);
-
-//router.get("/users", authMiddleware, authController.getUsers);
-//router.get("/users", roleMiddleware(["ADMIN"]), authController.getUsers);
 
 module.exports = router;

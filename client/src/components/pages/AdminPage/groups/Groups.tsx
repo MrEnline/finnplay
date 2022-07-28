6c77f4d488c1be28d24a3def90f7cce833e1a6d1
Sergey Kuznetsov <mr.enline@gmail.com>
2022-07-28 18:28:19 +0300

    continue release admin page

--- client/src/components/pages/AdminPage/groups/Groups.tsx
+++ client/src/components/pages/AdminPage/groups/Groups.tsx
@@ -1,13 +1,21 @@
 import { FC } from 'react';
 import styles from './Groups.module.css';
 import { TypeGroup } from '../../../../utils/Interfaces';
+import IconAdd from '../../../../assets/img/icon-add.svg';
 
 interface TypeProp {
     groups: Array<TypeGroup>;
 }
 
 const Groups: FC<TypeProp> = ({ groups }) => {
-    return <div className={styles.app__groups}></div>;
+    return (
+        <div className={styles.app__groups}>
+            <div className={styles.groups__title}>
+                <span className={styles.groups__titletext}>Groups</span>
+                <img onClick={() => console.log('add group')} alt="add" src={IconAdd} />
+            </div>
+        </div>
+    );
 };
 
 export default Groups;
